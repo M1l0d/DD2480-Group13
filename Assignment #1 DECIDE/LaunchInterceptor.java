e214a68b1f4b6d4c05c2a2c03dae71195e882373
Alexander Widman <119602587+AlexWidman@users.noreply.github.com>
2024-01-26 18:46:55 +0100

    feat(lic0): implement lic0 fix #1
    - Implemented solution for lic0
    - Implemented general function for calculating distance between two points.
    - Tested lic0

--- Assignment #1 DECIDE/LaunchInterceptor.java
+++ Assignment #1 DECIDE/LaunchInterceptor.java
@@ -1,9 +1,9 @@
 public class LaunchInterceptor {
     // CONSTANT
-    private static final double PI = 3.1415926535;
+    public static final double PI = 3.1415926535;
 
     // TYPE DECLARATIONS
-    private enum Connectors { 
+    public enum Connectors { 
         NOTUSED(777),
         ORR(778),
         ANDD(779); 
@@ -14,7 +14,7 @@ public class LaunchInterceptor {
         }
     }
 
-    private enum CompType { 
+    public enum CompType { 
         LT(1111),
         EQ(1112), 
         GT(1113); 
@@ -26,7 +26,7 @@ public class LaunchInterceptor {
     }
 
     // INPUTS TO THE DECIDE() FUNCTION
-    private static class Parameters {
+    public static class Parameters {
         double LENGTH1;
         double RADIUS1;
         double EPSILON;
@@ -49,43 +49,43 @@ public class LaunchInterceptor {
     }
 
     // GLOBAL VARIABLE DECLARATIONS
-    private Parameters parameters = new Parameters();
-    private static Parameters parameters2 = new Parameters();
+    public Parameters parameters = new Parameters();
+    public static Parameters parameters2 = new Parameters();
 
     // X coordinates of data points
-    private double[] x = new double[100];
-    private static double[] x2 = new double[100];
+    public double[] x;
+    public static double[] x2;
     // Y coordinates of data points
 
-    private double[] y = new double[100];    
-    private static double[] y2 = new double[100];    
+    public double[] y;    
+    public static double[] y2;    
 
     // Number of data points
-    private int numPoints;
-    private static int numPoints2;
+    public int numPoints;
+    public static int numPoints2;
 
     // Logical Connector Matrix
-    private Connectors[][] lcm = new Connectors[15][15];
-    private static Connectors[][] lcm2 = new Connectors[15][15];
+    public Connectors[][] lcm = new Connectors[15][15];
+    public static Connectors[][] lcm2 = new Connectors[15][15];
 
     // Preliminary Unlocking Matrix
-    private boolean[][] pum = new boolean[15][15];
-    private static boolean[][] pum2 = new boolean[15][15];
+    public boolean[][] pum = new boolean[15][15];
+    public static boolean[][] pum2 = new boolean[15][15];
 
     // Conditions Met Vector
-    private boolean[] cmv = new boolean[15];
-    private static boolean[] cmv2 = new boolean[15];
+    public boolean[] cmv = new boolean[15];
+    public static boolean[] cmv2 = new boolean[15];
 
     // Final Unlocking Vector
-    private boolean[] fuv = new boolean[15];
-    private static boolean[] fuv2 = new boolean[15];
+    public boolean[] fuv = new boolean[15];
+    public static boolean[] fuv2 = new boolean[15];
 
     // Decision: Launch or No Launch
-    private boolean launch;
-    private static boolean launch2;
+    public boolean launch;
+    public static boolean launch2;
 
     // Compare floating-point numbers
-    private static CompType doubleCompare(double a, double b) {
+    public static CompType doubleCompare(double a, double b) {
         if (Math.abs(a - b) < 0.000001)
             return CompType.EQ;
         if (a < b)
@@ -94,12 +94,40 @@ public class LaunchInterceptor {
     }
 
     // Function you must write
-    private static void decide() {
+    public static void decide() {
         // Your implementation here
     }
 
     // SKRIV FUNKTIONER NEDAN
 
+    // There exists at least one set of two consecutive data points that are a distance greater than
+    // the length, LENGTH1, apart.
+    // (0 ≤ LENGTH1)
+
+    /**
+     * Calculates distance between two points
+     * @param xCord1 x-coordinate for point 1
+     * @param xCord2 x-coordinate for point 2
+     * @param yCord1 y-coordinate for point 1
+     * @param yCord2 y-coordinate for point 2
+     * @return a double that is the distance
+     */
+    public double calcDistance(double xCord1, double xCord2, double yCord1, double yCord2){
+        return Math.sqrt(Math.pow(xCord1-xCord2, 2) + Math.pow(yCord1-yCord2, 2));
+    }
+
+    /**
+    * Sees if there exists at least one set of two consecutive data points that are a distance greater than LENGTH1 apart.
+    * @return true if it exists, false if it doesn't
+    */
+    public boolean lic0(){
+        for(int i = 0; i < numPoints-1; i++){
+            if(calcDistance(x[i], x[i+1], y[i], y[i+1]) > parameters.LENGTH1){
+                return true;
+            }
+        }
+        return false;
+    }
 
 
     // Main method
---
+++ Assignment #1 DECIDE/LaunchInterceptorTest.java
@@ -0,0 +1,160 @@
+import java.util.HashSet;
+import java.util.Random;
+import java.util.Set;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class LaunchInterceptorTest {
+    private LaunchInterceptor emptyLI;
+    private LaunchInterceptor randomLI;
+    private static int randomBoundOnParameters = 50;
+    private static int numPoints = 200;
+    
+    @Before
+    public void setUp() {
+        randomLI = generateLaunchInterceptorWithRandomParameters(numPoints, randomBoundOnParameters);
+        emptyLI = generateEmptyLaunchInterceptor();
+    }
+
+    public LaunchInterceptor generateLaunchInterceptorWithRandomParameters(int numPoints, int randomBoundOnParameters) {
+        LaunchInterceptor li = new LaunchInterceptor();
+
+        Random r = new Random();
+        // Gives every parameter a random value between 0-randomBoundOnParameters
+        li.parameters.LENGTH1 = r.nextInt(randomBoundOnParameters);
+        li.parameters.RADIUS1 = r.nextInt(randomBoundOnParameters);
+        li.parameters.EPSILON = r.nextInt(randomBoundOnParameters);
+        li.parameters.AREA1 = r.nextInt(randomBoundOnParameters);
+        li.parameters.QPTS = r.nextInt(randomBoundOnParameters);
+        li.parameters.QUADS = r.nextInt(randomBoundOnParameters);
+        li.parameters.DIST = r.nextInt(randomBoundOnParameters);
+        li.parameters.NPTS = r.nextInt(randomBoundOnParameters);
+        li.parameters.KPTS = r.nextInt(randomBoundOnParameters);
+        li.parameters.APTS = r.nextInt(randomBoundOnParameters);
+        li.parameters.BPTS = r.nextInt(randomBoundOnParameters);
+        li.parameters.CPTS = r.nextInt(randomBoundOnParameters);
+        li.parameters.DPTS = r.nextInt(randomBoundOnParameters);
+        li.parameters.EPTS = r.nextInt(randomBoundOnParameters);
+        li.parameters.FPTS = r.nextInt(randomBoundOnParameters);
+        li.parameters.GPTS = r.nextInt(randomBoundOnParameters);
+        li.parameters.LENGTH2 = r.nextInt(randomBoundOnParameters);
+        li.parameters.RADIUS2 = r.nextInt(randomBoundOnParameters);
+        li.parameters.AREA2 = r.nextInt(randomBoundOnParameters);
+
+        li.numPoints = numPoints;
+        li.x = new double[numPoints];
+        li.y = new double[numPoints];
+        
+        // Creates random coordinates and assures that there are no duplicates 
+        Set<int[]> coordinateSet = new HashSet<>();
+        int upperBound = 100;
+
+        for(int i = 0; i < numPoints; i++) {
+            
+            int randX = r.nextInt(upperBound);
+            int randY = r.nextInt(upperBound);
+            int[] coordPair = new int[]{randX, randY};
+
+            // .add() returns false if there already exists an equal coordPAir in the set, if so generates a new Coordpair until uniqe one is found
+            while(!coordinateSet.add(coordPair)) {
+                randX = r.nextInt(upperBound);
+                randX = r.nextInt(upperBound);
+                coordPair = new int[]{randX, randY};
+            }
+            
+            coordinateSet.add(coordPair);
+            li.x[i] = coordPair[0];
+            li.y[i] = coordPair[1];
+        }
+
+        return li;
+    }
+
+    public LaunchInterceptor generateEmptyLaunchInterceptor() {
+        return new LaunchInterceptor();
+    }
+    
+    /* Detta kan man köra bara för att testa sin funktion och se vad som sker med random data */
+    @Test 
+    public void printOutLaunchInterceptorRandomDataToTestFunctionOfChoice() {
+        
+        // Exempel
+        //System.out.println("Funktion LIC1 med random data: " + randomLI.lic1());
+        
+        // Avkommentera för att se data
+        // System.out.println("Amount of coordninates: " + coordinateSet.size());
+        // for(int[] coordEntry : coordinateSet) {
+        //     System.out.println("(" + coordEntry[0] + ", " + coordEntry[1] + ")");
+        // }
+
+        // System.out.println("\nParameters: "); 
+        // System.out.println(" LENGTH1 " + li.parameters.LENGTH1);
+        // System.out.println(" RADIUS1 " + li.parameters.RADIUS1);
+        // System.out.println(" EPSILON " + li.parameters.EPSILON);
+        // System.out.println(" AREA1 " + li.parameters.AREA1);
+        // System.out.println(" QPTS " + li.parameters.QPTS);
+        // System.out.println(" QUADS " + li.parameters.QUADS);
+        // System.out.println(" DIST " + li.parameters.DIST);
+        // System.out.println(" NPTS " + li.parameters.NPTS);
+        // System.out.println(" KPTS " + li.parameters.KPTS);
+        // System.out.println(" APTS " + li.parameters.APTS);
+        // System.out.println(" BPTS " + li.parameters.BPTS);
+        // System.out.println(" CPTS " + li.parameters.CPTS);
+        // System.out.println(" DPTS " + li.parameters.DPTS);
+        // System.out.println(" EPTS " + li.parameters.EPTS);
+        // System.out.println(" FPTS " + li.parameters.FPTS);
+        // System.out.println(" GPTS " + li.parameters.GPTS);
+        // System.out.println(" LENGTH2 " + li.parameters.LENGTH2);
+        // System.out.println(" RADIUS2 " + li.parameters.RADIUS2);
+        // System.out.println(" AREA2 " + li.parameters.AREA2);
+    }
+
+    /*
+    @Test
+    public void lic1ReturnsTrueIfThreeConsecutiveDataPointsWithinRadius() {
+        double[] xCoordinates = new double[]{1,1,1,50,60,70,80};
+        double[] yCoordinates = new double[]{2,3,4,50,60,70,80};
+        double radius = 10;
+
+        emptyLI.parameters.RADIUS1 = radius;
+        emptyLI.numPoints = xCoordinates.length;
+        emptyLI.x = xCoordinates;
+        emptyLI.y = yCoordinates;
+
+        assertTrue(emptyLI.lic1());
+    }*/
+
+    @Test
+    public void lic0ReturnsFalseIfDistanceBetweeenTwoConsecutiveDataPointsLessThanLENGTH1(){
+        double[] xCoordinates = new double[]{1,1,4,7,10};
+        double[] yCoordinates = new double[]{1,1,5,8,10};
+        double length1 = 5;
+        // if x1=1, x2=4, y1=1, y2=5, then distance will be exactly 5
+        // this should return false as distance needs to be strictly more than LENGTH1
+
+        emptyLI.parameters.LENGTH1 = length1;
+        emptyLI.numPoints = xCoordinates.length;
+        emptyLI.x = xCoordinates;
+        emptyLI.y = yCoordinates;
+
+        assertFalse(emptyLI.lic0());
+    }
+
+    @Test
+    public void lic0ReturnsTrueIfDistanceBetweeenTwoConsecutiveDataPointsMoreThanLENGTH1(){
+        double[] xCoordinates = new double[]{1,5,10,20,100};
+        double[] yCoordinates = new double[]{1,5,10,20,100};
+        double length1 = 5;
+
+        emptyLI.parameters.LENGTH1 = length1;
+        emptyLI.numPoints = xCoordinates.length;
+        emptyLI.x = xCoordinates;
+        emptyLI.y = yCoordinates;
+
+        assertTrue(emptyLI.lic0());
+    }
+}
+
