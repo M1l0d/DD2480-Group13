e477350bf4ac2f4e0868de302be0261e37a3344c
Annie Kihlert <kihlert@kth.se>
2024-01-31 16:00:58 +0100

    feat(lic3, lic8, lic13): Implemented solutions and tests for  lic3, lic8, lic13 Closes #4, Closes #9, Closes #14

--- Assignment #1 DECIDE/LaunchInterceptor.java
+++ Assignment #1 DECIDE/LaunchInterceptor.java
@@ -1,3 +1,4 @@
+import java.applet.AppletStub;
 import java.lang.Math;
 import java.util.Arrays;
 import java.util.HashSet;
@@ -111,5 +112,195 @@ public class LaunchInterceptor {
     }
 
     // SKRIV FUNKTIONER NEDAN
+    /* LIC 3
+    There exists at least one set of three 
+    consecutive data points that are the vertices of a triangle
+    area greater than AREA1. */
+public boolean LIC3() {
+    for (int i = 0, j = 0; i < x.length - 2 && j < y.length - 2; i++, j++) {
+        double x1 = x[i];
+        double y1 = y[j];
+        double x2 = x[i + 1];
+        double y2 = y[j + 1];
+        double x3 = x[i + 2];
+        double y3 = y[j + 2];
+        
+        double area = calculateTriangleArea(x1, y1, x2, y2, x3, y3);
+        if (area > parameters.AREA1) {
+            return true;
+        } 
+    } return false;
+}
+//checks that the points form a triangle
+public static double calculateTriangleArea(double x1, double y1, double x2, double y2, double x3, double y3) {
+    return Math.abs((x1 * (y2 - y3) + x2 * (y3 - y1) + x3 * (y1 - y2)) / 2);
+}
+/*Issue 9. There exists at least one set of three data points 
+* separated by exactly A PTS and B PTS consecutive intervening points, 
+* respectively, that cannot be contained within or on a circle of 
+* radius RADIUS1. The condition is not met when NUMPOINTS < 5.
+* 1 ≤ A PTS, 1 ≤ B PTS
+* A PTS+B PTS ≤ (NUMPOINTS−3)
+* @param x1 - x coordinate of first point
+* @param y1 - y coordinate of first point
+* @param x2 - x coordinate of second point
+* @param y2 - y coordinate of second point
+* @param x3 - x coordinate of third point
+* @param y3 - y coordinate of third point
+*
+*/
+
+public boolean LIC8() {
+    /*CONDITION-CHECKS
+    // Check if NUMPOINTS is less than 5
+    if (numPoints < 5) {
+        return false; // Condition not met
+    }
+    // Check if A and B are less than 1
+    if (parameters.APTS < 1 || parameters.BPTS < 1) {
+        return false; // Condition not met
+    }
+    // Check if A + B is greater than NUMPOINTS - 3
+    if (parameters.APTS + parameters.BPTS > numPoints - 3) {
+        return false; // Condition not met
+    // }
+    /* Check if there exists at least one set of three data points separated by 
+    exactly A_PTS and B_PTS consecutive intervening points, respectively,
+    that cannot be contained within or on a circle of radius RADIUS1*/
+    for (int i = 0; i < numPoints - parameters.APTS-parameters.BPTS-2; i++) { // NUMPOINTS - 2 to ensure there's remaining data points for an iteration
+        double x1 = x[i];
+        double y1 = y[i];
+        double x2 = x[i + parameters.APTS + 1];
+        double y2 = y[i + parameters.APTS + 1];
+        double x3 = x[i + parameters.APTS + parameters.BPTS + 2];
+        double y3 = y[i + parameters.APTS + parameters.BPTS + 2];
+
+
+
+        double inf = 1e18;
+        double minRadius = inf;
+
+        double[] midPoint1 = new double[]{x1 - x2 / 2, y1 - y2 / 2};
+        double radius1 = distBetweenPoint(x1, x2, y1, y2) / 2;
+        double[] lastPoint1 = new double[]{x3, y3};
+        boolean last1InCircle = pointInsideCircle(midPoint1, radius1, lastPoint1);
+        
+        if(last1InCircle && radius1 < minRadius) {
+            minRadius = radius1;
+        }
+
+        double[] midPoint2 = new double[]{x1 - x3 / 2, y1 - y3 / 2};
+        double radius2 = distBetweenPoint(x1, x3, y1, y3) / 2;
+        double[] lastPoint2 = new double[]{x2, y2};
+        boolean last2InCircle = pointInsideCircle(midPoint2, radius2, lastPoint2);
+
+        if(last2InCircle && radius2 < minRadius) {
+            minRadius = radius2;
+        }
+
+        double[] midPoint3 = new double[]{x2 - x3 / 2, y2 - y3 / 2};
+        double radius3 = distBetweenPoint(x2, x3, y2, y3) / 2;
+        double[] lastPoint3 = new double[]{x1, y1};
+        boolean last3InCircle = pointInsideCircle(midPoint3, radius3, lastPoint3);
+
+        if(last3InCircle && radius3 < minRadius) {
+            minRadius = radius3;
+        }
+
+        if(minRadius == inf) {
+            minRadius = calculateCircumCircleRadius(x1, x2, x3, y1, y2, y3);
+        }
+
+        if(minRadius > parameters.RADIUS1) {
+            return true;
+        } 
+    } return false;
+    }
+
+    public double calculateCircumCircleRadius(double xCord1, double xCord2, double xCord3, double yCord1, double yCord2, double yCord3) {        
+        double a = distBetweenPoint(xCord1, xCord2, yCord1, yCord2);
+        double b = distBetweenPoint(xCord1, xCord3, yCord1, yCord3);
+        double c = distBetweenPoint(xCord2, xCord3, yCord2, yCord3);
+        return (a*b*c) / Math.sqrt((a+b+c)*(b+c-a)*(c+a-b)*(a+b-c));
+    }
+
+    public double distBetweenPoint(double xCord1, double xCord2, double yCord1, double yCord2) {
+        return Math.sqrt(Math.pow(xCord1-xCord2, 2) + Math.pow(yCord1-yCord2, 2));   
+    }
+
+    public boolean pointInsideCircle(double[] midPointOfCircle, double radius, double[] point) {
+        return  (distBetweenPoint(midPointOfCircle[0], point[0], midPointOfCircle[1], point[1]) / 2) < radius;
+    }
+
+/* Issue 14. There exists at least one set of three data points, 
+separated by exactly A PTS and B PTS consecutive intervening points, 
+respectively, that cannot be contained within or on a circle of 
+radius RADIUS1. In addition, there exists at least one set of three 
+data points (which can be the same or different from the three data points 
+just mentioned) separated by exactly A PTS and B PTS consecutive intervening points,
+respectively, that can be contained in or on a circle of radius RADIUS2. 
+Both parts must be true for the LIC to be true. The condition is not met when NUMPOINTS < 5.
+0 ≤ RADIUS2*/
+//public static boolean LIC13(double[] x, double[] y, int APTS, int BPTS, int NUMPOINTS, double RADIUS1, double RADIUS2/
+public boolean LIC13() {
+    //CONDITION-CHECKS
+    // Check if NUMPOINTS is less than 5
+    if (numPoints < 5) {
+        return false; // Condition not met
+    }
+    // Check if RADIUS2 is less than 0
+    if (parameters.RADIUS2 < 0) {
+        return false; // Condition not met
+    }
+    if (LIC8()) { //Check that the first part of the condition is met
+        for (int i = 0; i < numPoints - 2; i++) { // NUMPOINTS - 2 to ensure there's remaining data points for an iteration
+            double x1 = x[i];
+            double y1 = y[i];
+            double x2 = x[i + parameters.APTS + 1];
+            double y2 = y[i + parameters.APTS + 1];
+            double x3 = x[i + parameters.APTS + parameters.BPTS + 2];
+            double y3 = y[i + parameters.APTS + parameters.BPTS + 2];
     
+           
+        double inf = 1e18;
+        double minRadius = inf;
+
+        double[] midPoint1 = new double[]{x1 - x2 / 2, y1 - y2 / 2};
+        double radius1 = distBetweenPoint(x1, x2, y1, y2) / 2;
+        double[] lastPoint1 = new double[]{x3, y3};
+        boolean last1InCircle = pointInsideCircle(midPoint1, radius1, lastPoint1);
+        
+        if(last1InCircle && radius1 < minRadius) {
+            minRadius = radius1;
+        }
+
+        double[] midPoint2 = new double[]{x1 - x3 / 2, y1 - y3 / 2};
+        double radius2 = distBetweenPoint(x1, x3, y1, y3) / 2;
+        double[] lastPoint2 = new double[]{x2, y2};
+        boolean last2InCircle = pointInsideCircle(midPoint2, radius2, lastPoint2);
+
+        if(last2InCircle && radius2 < minRadius) {
+            minRadius = radius2;
+        }
+
+        double[] midPoint3 = new double[]{x2 - x3 / 2, y2 - y3 / 2};
+        double radius3 = distBetweenPoint(x2, x3, y2, y3) / 2;
+        double[] lastPoint3 = new double[]{x1, y1};
+        boolean last3InCircle = pointInsideCircle(midPoint3, radius3, lastPoint3);
+
+        if(last3InCircle && radius3 < minRadius) {
+            minRadius = radius3;
+        }
+
+        if(minRadius == inf) {
+            minRadius = calculateCircumCircleRadius(x1, x2, x3, y1, y2, y3);
+        }
+
+        if(minRadius > parameters.RADIUS1) {
+            return true;
+        } 
+    } 
+    } return false;
+} 
+
 }
--- Assignment #1 DECIDE/LaunchInterceptorTest.java
+++ Assignment #1 DECIDE/LaunchInterceptorTest.java
@@ -112,6 +112,200 @@ public class LaunchInterceptorTest {
         // System.out.println(" AREA2 " + li.parameters.AREA2);
     }
 
+    //TESTS FOR LIC3
+    @Test
+    public void testLIC3TriangleWithGreaterAreaThanAREA1() { //test for LIC3
+        // Test case 1: Triangle with area greater than AREA1
+        //should return true
+        double[] x1 = {0.0, 1.0, 2.0};
+        double[] y1 = {0.0, 1.0, 0.0};
+        double area1 = 0.5; // smaller than the actual area
+        emptyLI.numPoints = x1.length;
+        emptyLI.parameters.AREA1 = area1;
+        emptyLI.x = x1;
+        emptyLI.y = y1;
+        assertTrue(emptyLI.LIC3());
+    }
+
+    @Test
+    public void testLIC3TriangleWithLessAreaThanAREA1() { //test for LIC3
+        // Test case 2: Triangle with area less than AREA1
+        // should return false
+        double[] x2 = {0.0, 1.0, 2.0};
+        double[] y2 = {0.0, 0.0, 0.0};
+        double area2 = 2.0; // larger than the actual area
+        emptyLI.parameters.AREA1 = area2;
+        emptyLI.numPoints = x2.length;
+        emptyLI.x = x2;
+        emptyLI.y = y2;
+        assertFalse(emptyLI.LIC3());
+    }
+    @Test
+    public void testLIC3TriangleWithGreaterAreaThanAREA1AndNotEnoughPoints() { //test for LIC3
+        // Test case 3: Not enough points for a triangle
+        //should return false
+        double[] x3 = {0.0, 1.0};
+        double[] y3 = {0.0, 1.0};
+        double area3 = 1.0;
+        emptyLI.numPoints = x3.length;
+        emptyLI.x = x3;
+        emptyLI.y = y3;
+        emptyLI.parameters.AREA1 = area3;
+        assertFalse(emptyLI.LIC3());
+    }
+
+    //TESTS FOR LIC8
+    @Test
+    public void testLIC8ConditionsMetandCannotBeContained() {
+        // Test case 1: Conditions met, data points cannot be contained in or on a circle of radius RADIUS1
+        double[] x1 = {0.0, 1.0, 2.0, 3.0, 4.0};
+        double[] y1 = {0.0, 1.0, 2.0, 1.0, 0.0};
+        emptyLI.x = x1;
+        emptyLI.y = y1;
+        int A = 1;
+        int B = 1;
+        emptyLI.parameters.APTS = A;
+        emptyLI.parameters.BPTS = B;
+        int NUMPOINTS = x1.length;
+        emptyLI.numPoints = NUMPOINTS;
+        double RADIUS1 = 0.5;
+        emptyLI.parameters.RADIUS1 = RADIUS1;
+        //assertFalse(emptyLI.LIC8());
+        assertTrue(emptyLI.LIC8());
+    }
+    @Test
+    public void testLIC8ConditionNotMetnumPointsLessThan5() {
+        // Test case 2: Conditions not met, NUMPOINTS < 5
+        double[] x2 = {0.0, 1.0, 2.0};
+        double[] y2 = {0.0, 1.0, 0.0};
+        emptyLI.x = x2;
+        emptyLI.y = y2;
+        int A2 = 2;
+        int B2 = 1;
+        emptyLI.parameters.APTS = A2;
+        emptyLI.parameters.BPTS = B2;
+        int NUMPOINTS2 = 3; // Less than 5
+        emptyLI.numPoints = NUMPOINTS2;
+        double RADIUS1_2 = 0.5;
+        emptyLI.parameters.RADIUS1 = RADIUS1_2;
+        assertFalse(emptyLI.LIC8());
+    }
+    @Test
+    public void testLIC8nConditionNotMetAandBLessThanOne() {
+        // Test case 3: Conditions not met, A or B is less than 1
+        double[] x3 = {0.0, 1.0, 2.0, 3.0, 4.0};
+        double[] y3 = {0.0, 1.0, 0.0, 1.0, 0.0};
+        emptyLI.x = x3;
+        emptyLI.y = y3;
+        int A3 = 0; // Less than 1
+        int B3 = 2;
+        emptyLI.parameters.APTS = A3;
+        emptyLI.parameters.BPTS = B3;
+        int NUMPOINTS3 = 5;
+        emptyLI.numPoints = NUMPOINTS3;
+        double RADIUS1_3 = 0.5;
+        emptyLI.parameters.RADIUS1 = RADIUS1_3;
+        assertFalse(emptyLI.LIC8());
+    }
+
+    @Test
+    public void testLIC8ConditionNotMetAplusB() {
+        // Test case 4: Conditions not met, A + B > NUMPOINTS - 3
+        double[] x4 = {0.0, 1.0, 2.0, 3.0, 4.0};
+        double[] y4 = {0.0, 1.0, 0.0, 1.0, 0.0};
+        emptyLI.x = x4;
+        emptyLI.y = y4;
+        int A4 = 3;
+        int B4 = 2;
+        emptyLI.parameters.APTS = A4;
+        emptyLI.parameters.BPTS = B4;
+        int NUMPOINTS4 = 5; // A + B > 2
+        emptyLI.numPoints = NUMPOINTS4;
+        double RADIUS1_4 = 0.5;
+        emptyLI.parameters.RADIUS1 = RADIUS1_4;
+        assertFalse(emptyLI.LIC8());
+    }
+
+    //TESTS FOR LIC8
+    @Test
+    public void testLIC13ConditionsMetPointsCannotContained() {
+        // Test case 1: Conditions met, data points cannot be contained in or on a circle of radius RADIUS1
+        double[] x1 = {0.0, 1.0, 2.0, 3.0, 4.0};
+        double[] y1 = {0.0, 1.0, 0.0, 1.0, 0.0};
+        emptyLI.x = x1;
+        emptyLI.y = y1;
+        int A1 = 1;
+        int B1 = 1;
+        emptyLI.parameters.APTS = A1;
+        emptyLI.parameters.BPTS = B1;
+        int NUMPOINTS = 5;
+        emptyLI.numPoints = NUMPOINTS;
+        double RADIUS1 = 0.5;
+        emptyLI.parameters.RADIUS1 = RADIUS1;
+        double RADIUS2 = 1.0;
+        emptyLI.parameters.RADIUS2 = RADIUS2;
+        assertTrue(emptyLI.LIC13());
+    }
+    @Test
+    public void testLIC13ConditionsNotMetnumPointsLessThanFive() {
+        // Test case 2: Conditions not met, NUMPOINTS < 5
+        double[] x2 = {0.0, 1.0, 2.0};
+        double[] y2 = {0.0, 1.0, 0.0};
+        emptyLI.x = x2;
+        emptyLI.y = y2;
+        int A2 = 2;
+        int B2 = 1;
+        emptyLI.parameters.APTS = A2;
+        emptyLI.parameters.BPTS = B2;
+        int NUMPOINTS2 = 3; // Less than 5
+        emptyLI.numPoints = NUMPOINTS2;
+        double RADIUS1_2 = 0.5;
+        emptyLI.parameters.RADIUS1 = RADIUS1_2;
+        double RADIUS2_2 = 1.0;
+        emptyLI.parameters.RADIUS2 = RADIUS2_2;
+        assertFalse(emptyLI.LIC13());
+    }
+    @Test
+    public void testLIC13ConditionsNotMetAandBLessThanOne() {
+        // Test case 3: Conditions not met, A or B is less than 1
+        double[] x3 = {0.0, 1.0, 2.0, 3.0, 4.0};
+        double[] y3 = {0.0, 1.0, 0.0, 1.0, 0.0};
+        emptyLI.x = x3;
+        emptyLI.y = y3;
+        int A3 = 0; // Less than 1
+        int B3 = 2;
+        emptyLI.parameters.APTS = A3;
+        emptyLI.parameters.BPTS = B3;
+        int NUMPOINTS3 = 5;
+        emptyLI.numPoints = NUMPOINTS3;
+        double RADIUS1_3 = 0.5;
+        emptyLI.parameters.RADIUS1 = RADIUS1_3;
+        double RADIUS2_3 = -1.0;
+        emptyLI.parameters.RADIUS2 = RADIUS2_3;
+        assertFalse(emptyLI.LIC13());
+    }
+        
+    @Test
+    public void testLIC13ConditionsNotMetAplusB() {
+        // Test case 4: Conditions not met, A + B > NUMPOINTS - 3
+        double[] x4 = {0.0, 1.0, 2.0, 3.0, 4.0};
+        double[] y4 = {0.0, 1.0, 0.0, 1.0, 0.0};
+        emptyLI.x = x4;
+        emptyLI.y = y4;
+        int A4 = 3;
+        int B4 = 2;
+        emptyLI.parameters.APTS = A4;
+        emptyLI.parameters.BPTS = B4;
+        int NUMPOINTS4 = 5; // A + B > 2
+        emptyLI.numPoints = NUMPOINTS4;
+        double RADIUS1_4 = 0.5;
+        emptyLI.parameters.RADIUS1 = RADIUS1_4;
+        double RADIUS2_4 = 1.0;
+        emptyLI.parameters.RADIUS2 = RADIUS2_4;
+        assertFalse(emptyLI.LIC13());
+    }
+
+
 
 }
 
